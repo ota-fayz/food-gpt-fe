bcd898ea8dc976a0f97726d1c6903b876b7d38c7
ota_fayz <ota_2411@mail.ru>
2025-03-26 20:22:44 +0100

    feat: welcome ui

--- src/pages/auth/welcome/page.tsx
+++ src/pages/auth/welcome/page.tsx
@@ -1,6 +1,28 @@
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+import { Container, Wrapper } from './style.ts'
+import ImageSalad from '../../../assets/images/salad_with_macronutrients.png'
+
 const Welcome = () => {
 	return (
-		<div>Welcome</div>
+		<Container>
+			<img className="img" src={ImageSalad} alt="Salad with macronutrients" />
+			<Wrapper>
+				<Typography variant="h4" align="center">
+					Легкий подсчет калорий с Al
+				</Typography>
+				<Typography variant="subtitle1" align="center">
+					Просто сделайте фото своего блюда, а Al сделает все остальное
+				</Typography>
+				<Typography variant="body1" align="center">
+					Получите персональный план за минуту
+				</Typography>
+				<Button variant="contained">
+					Начать
+				</Button>
+			</Wrapper>
+		</Container>
 	)
 }
 
---
+++ src/pages/auth/welcome/style.ts
@@ -0,0 +1,27 @@
+import { styled } from '@mui/material/styles'
+
+export const Container = styled('div')`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+
+    .img {
+        width: 100%;
+        height: auto;
+        display: block;
+        object-fit: cover;
+    }
+`
+
+export const Wrapper = styled('div')(({theme}) => ({
+	backgroundColor: theme.palette.common.black,
+	color: theme.palette.common.white,
+	borderTopLeftRadius: theme.spacing(4),
+	borderTopRightRadius: theme.spacing(4),
+	padding: theme.spacing(6),
+	height: '100%',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between'
+}))
